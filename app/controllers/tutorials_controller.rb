ad48d842022bc7f56af23622f3f30e85e01f3f5b
abuzar hamza <aubzarhamza@gmail.com>
2016-06-25 20:11:35 -0500

    updated new field

--- app/controllers/tutorials_controller.rb
+++ app/controllers/tutorials_controller.rb
@@ -1,9 +1,33 @@
 class TutorialsController < ApplicationController
 
 	def new
+		@tutorial = Tutorial.new
+		#@tutorial.build_sqldata
+		#@problem.questions.build
+		#3.times { @tutorial.questions.build } 
+	end
+
+	def create
+		@tutorial = Tutorial.new(tutorial_param)
+        #@tutorial.questions
+		if @tutorial.save
+          flash[:success] = "Your tutorial is submited for review"
+          redirect_to tutorial_path(@tutorial)
+		else
+		  puts @tutorial.errors.inspect
+          puts "fails"
+          render :new
+		end
 	end
 
 	def show
+		@tutorial = Tutorial.find(params[:id])
+
 	end
+
+	private
+        def tutorial_param
+            params.require(:tutorial).permit(:title, :problem , questions_attributes: [ :question , :answer , :hint])
+        end
 
 end
--- app/models/tutorial.rb
+++ app/models/tutorial.rb
@@ -3,4 +3,8 @@ class Tutorial < ActiveRecord::Base
 	has_many :questions
 	validates :title , presence: true, length: {minimum: 5 , maximum: 140}
 	validates :problem , presence: true , length: {minimum: 5 , maximum: 140}
+
+	accepts_nested_attributes_for :questions,
+                                :reject_if => :all_blank,
+                                :allow_destroy => true
 end
